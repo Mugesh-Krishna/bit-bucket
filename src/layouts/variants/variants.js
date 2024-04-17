a838af16ec998e9b150d54396ad9e9a7b476bfa8
Mugesh-Krishna <165270435+Mugesh-Krishna@users.noreply.github.com>
2024-04-17 10:43:33 +0530

    varient add

--- src/layouts/variants/variants.js
+++ src/layouts/variants/variants.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import {
   Container,
   Paper,
@@ -13,85 +13,118 @@ import {
   TableCell,
   TableContainer,
   TableHead,
-  TableRow
+  TableRow,
+  Collapse,
+  Link,
 } from "@mui/material";
-import { FaTrash } from "react-icons/fa";
-import Accordion from '@mui/material/Accordion';
-import AccordionSummary from '@mui/material/AccordionSummary';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-
 
 import MDButton from "components/MDButton";
+import Accordion from "@mui/material/Accordion";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 function Variants({ price }) {
   const [variants, setVariants] = useState([]);
   const [fields, setFields] = useState([""]);
   const [tableData, setTableData] = useState([]);
-  const [showTable, setShowTable] = useState(false); 
+  const [selectedValues, setSelectedValues] = useState([]);
+  const [showAddOptionsLink, setShowAddOptionsLink] = useState(true);
+  const [numFields, setNumFields] = useState(1);
+  const [showAddExtraDataInput, setShowAddExtraDataInput] = useState(false);
+  const [extraDataInputValues, setExtraDataInputValues] = useState([""]);
+  const [showAddExtraDataLink, setShowAddExtraDataLink] = useState(false);
 
+  const handleDoneData = () => {
+    const newData = [...tableData];
+
+    const additionalValues = {
+      variant: variants[variants.length - 1].medium,
+      newField: fields.filter((field) => field.trim() !== ""),
+      extraData: extraDataInputValues.filter((extra) => extra.trim() !== ""),
+    };
+
+    newData[newData.length - 1].additionalValues.push(additionalValues);
+
+    setTableData(newData);
+    setExtraDataInputValues([""]);
+    setShowAddExtraDataInput(false);
+  };
+
+  const handleAddExtraData = (e) => {
+    e.preventDefault();
+    setShowAddExtraDataInput(true);
+  };
+
+  const handleExtraDataInputChange = (e, index) => {
+    const { value } = e.target;
+    const newValues = [...extraDataInputValues];
+    newValues[index] = value;
+    setExtraDataInputValues(newValues);
+
+    if (value !== "" && index === extraDataInputValues.length - 1) {
+      setExtraDataInputValues([...newValues, ""]);
+    }
+  };
   const handleAddVariantOption = (e) => {
     e.preventDefault();
     setVariants([...variants, { size: "", medium: "" }]);
-    
     setFields([""]);
+    setShowAddOptionsLink(false);
   };
-  
 
   const handleNewFieldChange = (e, index) => {
     const newFields = [...fields];
     newFields[index] = e.target.value;
-  
     if (index === newFields.length - 1 && e.target.value.trim() !== "") {
-      
       newFields.push("");
+      setNumFields(numFields + 1);
     }
-  
     setFields(newFields);
   };
-  
-
-  const handleDeleteField = indexToDelete => {
-    setFields(fields.filter((value, index) => index !== indexToDelete));
-  };
-
-  const handleDeleteVariantOption = (index) => {
-    const updatedVariants = [...variants];
-    updatedVariants.splice(index, 1);
+  const handleDeleteField = (indexToDelete) => {
+    const updatedVariants = variants.filter(
+      (value, index) => index !== indexToDelete
+    );
+    const updatedFields = fields.filter(
+      (value, index) => index !== indexToDelete
+    );
     setVariants(updatedVariants);
-    const updatedFields = [...fields];
-    updatedFields.splice(index, 1);
     setFields(updatedFields);
   };
-
-  const handleVariantSizeChange = (e, index) => {
-    const updatedVariants = [...variants];
-    updatedVariants[index].size = e.target.value;
-    setVariants(updatedVariants);
-  };
-
   const handleVariantMediumChange = (e, index) => {
     const updatedVariants = [...variants];
     updatedVariants[index].medium = e.target.value;
     setVariants(updatedVariants);
   };
-
   const handleDone = (index) => {
     const newData = [...tableData];
     newData.push({
-      variant: variants[index].medium,
-      newField: [...fields],
-      price: price
+      variant: variants[index].size,
+      price: price,
+      additionalValues: [
+        {
+          variant: variants[index].medium,
+          newField: fields.filter((field) => field.trim() !== ""),
+        },
+      ],
     });
     setTableData(newData);
-
     const updatedVariants = [...variants];
     updatedVariants[index] = { size: "", medium: "" };
     setVariants(updatedVariants);
-
     setFields([""]);
-    setShowTable(true); // Set showTable to true after clicking the link
+    setShowAddExtraDataLink(true); // Show the "Add Extra Data" link
   };
 
+  const handleSelectChange = (e, index) => {
+    const selectedValue = e.target.value;
+    if (selectedValues.some((value) => value === selectedValue)) {
+      alert("Value already selected");
+    } else {
+      setSelectedValues([...selectedValues, selectedValue]);
+    }
+  };
   return (
     <Container maxWidth="md" style={{ marginTop: "20px" }}>
       <Paper elevation={3} style={{ padding: "20px", maxWidth: "500px" }}>
@@ -99,104 +132,219 @@ function Variants({ price }) {
           Variants
         </Typography>
         <Typography variant="h6" gutterBottom>
-          <a href="#" onClick={handleAddVariantOption}>
-            + Add Options like size or color
-          </a>
+          {showAddOptionsLink && (
+            <a href="#" onClick={handleAddVariantOption}>
+              + Add Options like size or color
+            </a>
+          )}
         </Typography>
 
         {variants.map((variant, index) => (
           <Grid container spacing={2} key={index} style={{ marginTop: "10px" }}>
-            <Grid container spacing={2}>
-              <Grid item xs={4} marginTop="15px">
-                <Select
-                  label="Types"
-                  variant="outlined"
-                  margin="normal"
-                  fullWidth
-                  value={variant.size}
-                  onChange={(e) => handleVariantSizeChange(e, index)}
-                  style={{ height: "80%" }}
-                >
-                  <MenuItem value="Small">Size</MenuItem>
-                  <MenuItem value="Medium">Style</MenuItem>
-                  <MenuItem value="Large">Color</MenuItem>
-                  <MenuItem value="X-Large">Material</MenuItem>
-                </Select>
-              </Grid>
-              <Grid item xs={4}>
-                <TextField
-                  label="Variant"
-                  variant="outlined"
-                  margin="normal"
-                  fullWidth
-                  value={variant.medium}
-                  onChange={(e) => handleVariantMediumChange(e, index)}
-                  style={{ height: "100%" }}
-                />
-              </Grid>
-              {variant.medium !== "" && fields.map((value, fieldIndex) => (
-                <Grid item xs={4} key={fieldIndex}>
-                  <TextField
-                    label="New Field"
-                    variant="outlined"
-                    margin="normal"
-                    fullWidth
-                    value={value}
-                    onChange={(e) => handleNewFieldChange(e, fieldIndex)}
-                    onBlur={() => {
-                      if (value.trim() === "") {
-                        handleDeleteField(fieldIndex);
-                      }
+            {!showAddExtraDataInput && (
+              <>
+                <Grid container spacing={2}>
+                  <Grid item xs={4} marginTop="15px">
+                    <Select
+                      label="Types"
+                      variant="outlined"
+                      margin="normal"
+                      fullWidth
+                      value={variant.size}
+                      onChange={(e) => handleSelectChange(e, index)}
+                      style={{ height: "80%" }}
+                    >
+                      <MenuItem value="Size">Size</MenuItem>
+                      <MenuItem value="Style">Style</MenuItem>
+                      <MenuItem value="Color">Color</MenuItem>
+                      <MenuItem value="Material">Material</MenuItem>
+                    </Select>
+                  </Grid>
+                  <Grid item xs={4}>
+                    <TextField
+                      label="Variant"
+                      variant="outlined"
+                      margin="normal"
+                      fullWidth
+                      value={variant.medium}
+                      onChange={(e) => handleVariantMediumChange(e, index)}
+                      style={{ height: "100%" }}
+                    />
+                  </Grid>
+                  {variant.medium !== "" &&
+                    fields.map((value, fieldIndex) => (
+                      <Grid item xs={3} key={fieldIndex}>
+                        {fieldIndex < numFields && (
+                          <TextField
+                            label="New Field"
+                            variant="outlined"
+                            margin="normal"
+                            fullWidth
+                            value={value}
+                            onChange={(e) =>
+                              handleNewFieldChange(e, fieldIndex)
+                            }
+                            onBlur={() => {
+                              if (value.trim() === "") {
+                                handleDeleteField(fieldIndex);
+                              }
+                            }}
+                            style={{ marginBottom: "10px" }}
+                          />
+                        )}
+                      </Grid>
+                    ))}
+                </Grid>
+                <Grid item xs={12}>
+                  <IconButton onClick={() => handleDone(index)}>
+                    Done
+                  </IconButton>
+                  <MDButton
+                    onClick={() => {
+                      handleDeleteField(index);
                     }}
-                    style={{ marginBottom: "10px" }}
-                  />
+                  >
+                    Delete
+                  </MDButton>
+                </Grid>
+              </>
+            )}
+            {showAddExtraDataLink && (
+              <Grid
+                container
+                justifyContent="flex-end"
+                style={{ marginTop: "10px" }}
+              >
+                <Grid item>
+                  <a href="#" onClick={handleAddExtraData}>
+                    + Add extra data
+                  </a>
+                </Grid>
+              </Grid>
+            )}
+            {showAddExtraDataInput &&
+              extraDataInputValues.map((value, index) => (
+                <Grid container style={{ marginTop: "10px" }} key={index}>
+                  <Grid item xs={12}>
+                    <TextField
+                      label="Extra Data"
+                      variant="outlined"
+                      margin="normal"
+                      fullWidth
+                      value={value}
+                      onChange={(e) => handleExtraDataInputChange(e, index)}
+                    />
+                  </Grid>
                 </Grid>
               ))}
-              <MDButton onClick={() => handleDone(index)}>Done</MDButton>
-              <IconButton onClick={() => handleDeleteVariantOption(index)}>
-                <FaTrash />
-              </IconButton>
-            </Grid>
+            {showAddExtraDataInput && (
+              <Grid container style={{ marginTop: "10px" }}>
+                <Grid item xs={12}>
+                  <MDButton
+                    variant="contained"
+                    color="primary"
+                    onClick={handleDoneData}
+                  >
+                    Done
+                  </MDButton>
+                </Grid>
+              </Grid>
+            )}
           </Grid>
         ))}
-        
-        {showTable && ( 
-          <TableContainer>
-            <Table>
-              <TableHead>
-                <TableRow>
-                  <TableCell>Variant</TableCell>
-                  <TableCell>Price</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {tableData.map((data, dataIndex) => (
-                  <TableRow key={dataIndex}>
-                      <Accordion>
-      <AccordionSummary
-        expandIcon={<ExpandMoreIcon />}
-        aria-controls="panel1a-content"
-        id="panel1a-header"
-      >
-        {data.variant}
-      </AccordionSummary>
-     
-        {data.newField.map((field, fieldIndex) => (
-          <div key={fieldIndex}>{field}</div>
-        ))}
-      
-    </Accordion>
 
-                    <TableCell>{data.price}</TableCell>
-                    <TableCell>
-                      
-                    </TableCell>
+        <Grid item xs={12}>
+          <Collapse in={tableData.length > 0}>
+            <TableContainer>
+              <Table>
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Variant</TableCell>
+                    <TableCell>Price</TableCell>
                   </TableRow>
+                </TableHead>
+                <TableBody>
+  {tableData.map((data, dataIndex) => (
+    <React.Fragment key={dataIndex}>
+      <TableRow>
+        <TableCell>{data.variant}</TableCell>
+        <TableCell>{price}</TableCell> {/* Display the price */}
+      </TableRow>
+      <TableRow>
+        <TableCell colSpan={2}>
+          <Accordion>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls={`panel-${dataIndex}-variant-content`}
+              id={`panel-${dataIndex}-variant-header`}
+            >
+              <Typography>{data.variant}</Typography>
+              <Typography variant="body2" color="textSecondary">
+                {data.variant}
+              </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Table>
+                <TableBody>
+                  {data.extraData &&
+                    data.extraData.map((extra, extraIndex) => (
+                      <TableRow
+                        key={`variant-${dataIndex}-extra-${extraIndex}`}
+                      >
+                        <TableCell colSpan={2}>
+                          {extra}
+                        </TableCell>
+                      </TableRow>
+                    ))}
+                </TableBody>
+              </Table>
+            </AccordionDetails>
+          </Accordion>
+        </TableCell>
+      </TableRow>
+      {data.additionalValues &&
+  data.additionalValues.map((additionalData, additionalIndex) => (
+    <React.Fragment key={`${dataIndex}-${additionalIndex}`}>
+      {additionalData.newField.map((field, fieldIndex) => (
+      <TableRow key={`${dataIndex}-${additionalIndex}-${fieldIndex}`}>
+      <TableCell colSpan={2}>
+        <Accordion>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls={`panel-${dataIndex}-${additionalIndex}-${fieldIndex}-content`}
+            id={`panel-${dataIndex}-${additionalIndex}-${fieldIndex}-header`}
+          >
+            <Typography>{field}</Typography>
+            <Typography variant="body2" color="textSecondary">
+              {price} {/* Display the price */}
+            </Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <div>
+              <Typography>Extra Data:</Typography>
+              <ul>
+                {additionalData.extraData && additionalData.extraData.map((extra, extraIndex) => (
+                  <li key={`extra-${extraIndex}`}>{extra}</li>
                 ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
-        )}
+              </ul>
+            </div>
+          </AccordionDetails>
+        </Accordion>
+      </TableCell>
+    </TableRow>
+    
+      ))}
+    </React.Fragment>
+  ))}
+
+    </React.Fragment>
+  ))}
+</TableBody>
+
+              </Table>
+            </TableContainer>
+          </Collapse>
+        </Grid>
       </Paper>
     </Container>
   );

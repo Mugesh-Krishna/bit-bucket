4c889530bc1a9dd0c5b3b5f0b2ac1156e3a3371f
Mugesh-Krishna <165270435+Mugesh-Krishna@users.noreply.github.com>
2024-04-17 11:49:03 +0530

    id

--- src/layouts/variants/variants.js
+++ src/layouts/variants/variants.js
@@ -19,9 +19,9 @@ import {
 } from "@mui/material";
 
 import MDButton from "components/MDButton";
-import Accordion from '@mui/material/Accordion';
-import AccordionSummary from '@mui/material/AccordionSummary';
-import AccordionDetails from '@mui/material/AccordionDetails';
+import Accordion from "@mui/material/Accordion";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import AccordionSummary from "@mui/material/AccordionSummary";
 
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 function Variants({ price }) {
@@ -311,8 +311,8 @@ function Variants({ price }) {
             <Accordion>
               <AccordionSummary
                 expandIcon={<ExpandMoreIcon />}
-                aria-controls={`panel-${dataIndex}-${additionalIndex}-${fieldIndex}-content`}
-                id={`panel-${dataIndex}-${additionalIndex}-${fieldIndex}-header`}
+                aria-controls="panel2-content"
+                id="panel2-header"
               >
                 <Typography>{field}</Typography>
                 <Typography variant="body2" color="textSecondary">

f48f675d576fb1df33543c9b37dd0227e6bf0e58
Mugesh-Krishna <165270435+Mugesh-Krishna@users.noreply.github.com>
2024-04-18 12:54:09 +0530

    final stage

--- src/layouts/variants/variants.js
+++ src/layouts/variants/variants.js
@@ -20,9 +20,9 @@ import {
 } from "@mui/material";
 
 import MDButton from "components/MDButton";
-import Accordion from "@mui/material/Accordion";
-import AccordionDetails from "@mui/material/AccordionDetails";
-import AccordionSummary from "@mui/material/AccordionSummary";
+import {Accordion} from "@mui/material";
+import {AccordionDetails} from "@mui/material";
+import {AccordionSummary} from "@mui/material";
 import { useEffect } from "react";
 
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
